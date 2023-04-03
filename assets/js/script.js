cf556e824f13cadaad21901e527487068db4b9ff
Sal8298 <salahuddinasim95@gmail.com>
2023-04-03 17:14:48 -0500

    Timer works when you press Start quiz. Start button vanishes but doesn't reappear yet.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,33 @@
 console.log('connected');
+
+//Html elements
+var timerEl = document.querySelector('#timer');
+var quizStartBtn = document.querySelector('#startQuiz');
+
+//Global variables
+var timeLeft;
+
+//Start Quiz Function
+quizStartBtn.addEventListener('click', startQuiz);
+
+function startQuiz() {
+    quizStartBtn.style.display = 'none';
+    timeLeft = 10;
+    timerEl.textContent = timeLeft;
+    startTimer();
+}
+
+
+//Timer function
+function startTimer() {
+var timer = setInterval(function() {
+    timeLeft--;
+    timerEl.textContent = timeLeft;
+    if (timeLeft <= 0) {
+        clearInterval(timer);
+       timerEl.textContent = 0
+       quizStartBtn.style.display = 'visible';
+    };
+}, 1000);
+};
+
