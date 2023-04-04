af5741dcda7e8c0c9836deeb96b98f81ec6c2556
Sal8298 <salahuddinasim95@gmail.com>
2023-04-04 11:48:36 -0500

    First question displays when you click start quiz.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
 console.log('connected');
 
 //Html elements
-var timerEl = document.querySelector('#timer');
-var quizStartBtn = document.querySelector('#startQuiz');
+const timerEl = document.querySelector('#timer');
+const quizStartBtn = document.querySelector('#startQuiz');
+const questionEL = document.querySelector('#question');
+const option1EL = document.querySelector('#option1');
+const option2EL = document.querySelector('#option2');
+const option3EL = document.querySelector('#option3');
+const option4EL = document.querySelector('#option4');
 
 //Global variables
-var timeLeft;
+let timeLeft;
+let questionArray = [
+    { question: 'What does HTML stand for?', 
+          option3: '3. Hypertext Markup Language',
+          options: [{options: '1. Hypertext Markdown Language'}, {options: '2. Hyper Mark Language'}, {options: '3. Hypertext Markup Language'}, {options: '4. None of the above'}]
+        },
+        { question: 'Inside which HTML element do we put the javascript?', 
+          option3: '3. <script>', 
+          options: [{options: '1. <h1>'}, {options: '2. <js>'}, {options: '3. <script>'}, {options: '4. <head>'}]
+        },
+    ]
+
 
 //Start Quiz Function
 quizStartBtn.addEventListener('click', startQuiz);
@@ -15,6 +31,7 @@ function startQuiz() {
     timeLeft = 10;
     timerEl.textContent = timeLeft;
     startTimer();
+    displayQuestions();
 }
 
 
@@ -30,4 +47,12 @@ var timer = setInterval(function() {
     };
 }, 1000);
 };
+
+function displayQuestions() {
+    questionEL.textContent = questionArray[0].question
+    option1EL.textContent = questionArray[0].options[0].options
+    option2EL.textContent = questionArray[0].options[1].options
+    option3EL.textContent = questionArray[0].options[2].options
+    option4EL.textContent = questionArray[0].options[3].options
+}
 
